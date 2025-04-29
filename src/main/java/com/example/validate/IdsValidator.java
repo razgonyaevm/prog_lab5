23121b4f2ca7a239a401edaa78b39b2bb2e47f87
razgonyaevm <razgonyaevm@gmail.com>
2025-04-29 18:36:24 +0300

    Used the Stream Api to set the flag and to search for the maximum id.

--- src/main/java/com/example/validate/IdsValidator.java
+++ src/main/java/com/example/validate/IdsValidator.java
@@ -2,9 +2,7 @@ package com.example.validate;
 
 import com.example.service.model.IdGenerator;
 import com.example.service.model.Movie;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class IdsValidator implements Validator<List<Movie>> {
   @Override
@@ -27,18 +25,16 @@ public class IdsValidator implements Validator<List<Movie>> {
 
   /* Метод для сброса генератора ID */
   private void resetIdGenerator(List<Movie> movies) {
-    long maxId = 0;
-    boolean hasIds = false;
 
-    // Ищем максимальный ID в загружаемой коллекции
-    for (Movie movie : movies) {
-      if (movie.getId() != null) {
-        hasIds = true;
-        if (movie.getId() > maxId) {
-          maxId = movie.getId();
-        }
-      }
-    }
+    // Ищем максимальный ID в загружаемой коллекции и устанавливаем флаг, если в файле есть movies с
+    // id
+    boolean hasIds = movies.stream().anyMatch(movie -> movie.getId() != null);
+    long maxId =
+        movies.stream()
+            .map(Movie::getId)
+            .filter(Objects::nonNull)
+            .max(Comparator.naturalOrder())
+            .orElse(0L);
 
     // Если нашли какие-нибудь ID, устанавливаем новое стартовое значение генератора
     if (hasIds) {
